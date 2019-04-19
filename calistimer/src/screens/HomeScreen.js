13e4cb0cabfae42d37816bd589378e6109fecac2
gferreira <gferreiraa@gmail.com>
2019-04-19 20:18:57 -0300

    Add app routers

--- calistimer/src/screens/HomeScreen.js
+++ calistimer/src/screens/HomeScreen.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 const HomeScreen = props => {
   return (
-    <View style={{ backgroundColor: '#D6304A'}}>
-      <TouchableHighlight onPress={ () => props.navigation.navigate('EMOM')}>
-        <Text>Home Screen</Text>
-      </TouchableHighlight>
+    <View style={{ flex: 1, backgroundColor: '#D6304A'}}>
+    <Text style={{ fontFamily: 'Ubuntu-Bold', fontSize: 48, textAlign: 'center', color: 'white', marginTop: 111}}>CalisTimer</Text>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
+        <Text style={{ color: 'white', fontSize:24, textAlign: 'center', fontFamily: 'Ubuntu-Regular'}}>EMOM</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
+        <Text>AMRAP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
+      <Text>ISOMETRIA</Text>
+    </TouchableOpacity>
     </View>
   )
 }
