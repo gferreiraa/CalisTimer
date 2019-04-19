bb0646583553c3cb408320a7c786731c6b5a5c13
gferreira <gferreiraa@gmail.com>
2019-04-18 23:37:21 -0300

    Created navigation

--- calistimer/App.js
+++ calistimer/App.js
@@ -1,33 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+/* React Navigation Imports */
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from './src/screens/homeScreen';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Calistimer</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen
+}, { initialRouteName: 'Home' })
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
