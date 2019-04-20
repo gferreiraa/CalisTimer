d63bcb6b1ca280d543f442f56f32ec41b6a6160b
gferreira <gferreiraa@gmail.com>
2019-04-20 16:01:25 -0300

    Refactoring navigation components

---
+++ calistimer/src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+
+const Button = props => {
+  return(
+    <TouchableOpacity onPress={props.onPress}>
+      <Text style={styles.text}>{props.children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: { 
+    color: 'white', 
+    fontSize: 24, 
+    textAlign: 'center', 
+    fontFamily: 'Ubuntu-Regular'}
+})
+
+
+export default Button 
+
--- calistimer/src/screens/HomeScreen.js
+++ calistimer/src/screens/HomeScreen.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text } from 'react-native'
+
+import Button from '../components/Button'
+
 
 const HomeScreen = props => {
   return (
     <View style={{ flex: 1, backgroundColor: '#D6304A'}}>
-    <Text style={{ fontFamily: 'Ubuntu-Bold', fontSize: 48, textAlign: 'center', color: 'white', marginTop: 111}}>CalisTimer</Text>
-      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
-        <Text style={{ color: 'white', fontSize:24, textAlign: 'center', fontFamily: 'Ubuntu-Regular'}}>EMOM</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
-        <Text>AMRAP</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={ () => props.navigation.navigate('EMOM')}>
-      <Text>ISOMETRIA</Text>
-    </TouchableOpacity>
+      <Text style={{ fontFamily: 'Ubuntu-Bold', fontSize: 48, textAlign: 'center', color: 'white', marginTop: 111}}>CalisTimer</Text>
+        <Button onPress={() => props.navigation.navigate('EMOM')}>EMOM</Button>
+        <Button onPress={() => props.navigation.navigate('EMOM')}>EMOM2</Button>
+        <Button onPress={() => props.navigation.navigate('EMOM')}>EMOM3</Button>
+        <Button onPress={() => props.navigation.navigate('EMOM')}>EMOM4</Button>
     </View>
   )
 }
