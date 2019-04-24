24b711024cccf4ee6814340b70308fd538a55642
gferreira <gferreiraa@gmail.com>
2019-04-23 23:07:31 -0300

    Created: Select Component

--- calistimer/src/screens/EMOMScreen.js
+++ calistimer/src/screens/EMOMScreen.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+const Select = props => {
+  return (
+    <View style={{ flex: 1}}>
+      <Text style={{ textAlign: 'center'}}> Label </Text>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
+        <Text style={{ color: 'white'}}>Option 01</Text>
+        <Text style={{ color: 'white'}}>Option 01</Text>
+        <Text style={{ color: 'white'}}>Option 01</Text>
+      </View>
+    </View>
+  )
+}
+
 const EMOMScreen = props => {
   return (
     <View style={Styles.container}>
       <Text>EMOM Screen</Text>
+      <Select></Select>
     </View>
   )
 }
@@ -14,7 +28,8 @@ EMOMScreen.navigationOptions = {
 const Styles = StyleSheet.create({
   container: {
     flex: 1, 
-    backgroundColor: '#D6304A'
+    backgroundColor: '#D6304A',
+    paddingTop: 200
   }
 })
 
