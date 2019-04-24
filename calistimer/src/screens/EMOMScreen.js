393fefec9d15dcf02d39b614e8ec63327058c5fc
gferreira <gferreiraa@gmail.com>
2019-04-23 23:39:17 -0300

    Finished: Selected Component

--- calistimer/src/screens/EMOMScreen.js
+++ calistimer/src/screens/EMOMScreen.js
@@ -1,18 +1,44 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const Select = props => {
   return (
     <View style={{ flex: 1}}>
-      <Text style={{ textAlign: 'center'}}> Label </Text>
-      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around'}}>
-        <Text style={{ color: 'white'}}>Option 01</Text>
-        <Text style={{ color: 'white'}}>Option 01</Text>
-        <Text style={{ color: 'white'}}>Option 01</Text>
+      <Text style={ styleSelect.label }> Label </Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around'}}>
+      <TouchableOpacity style={[ styleSelect.opt, styleSelect.optSelected ]}>
+        <Text style={ styleSelect.opt }>Option 01</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[ styleSelect.opt ]}>
+        <Text style={ styleSelect.opt }>Option 01</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[ styleSelect.opt ]}>
+        <Text style={ styleSelect.opt }>Option 01</Text>
+      </TouchableOpacity>
       </View>
     </View>
   )
 }
+
+const styleSelect = StyleSheet.create({
+  label: {
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24
+  }, 
+  opt: {
+    padding: 8
+  },
+  optSelected: {
+    backgroundColor: 'rgba(255,255,255,0.6)'
+  },
+  opt: {
+    color: 'white',
+    fontFamily: 'Ubuntu-Regular',
+    fontSize: 24
+  }
+})
 
 const EMOMScreen = props => {
   return (
